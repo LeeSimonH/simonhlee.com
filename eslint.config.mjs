ca554d9613f5235de6023a0fd5eb8d7be3fce8bc
LeeSimonH <simonlee1125@gmail.com>
2025-08-11 07:21:11 -0400

    chore(config): update config files
    - add .gitignore patterns for local files
    - update TypeScript and Next.js configs for .mdx files
    - (nit) prevent 'prettier/prettier' errors from erroring

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,7 @@ const eslintConfig = [
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      'prettier/prettier': 'warn',
     },
   }),
 ]
