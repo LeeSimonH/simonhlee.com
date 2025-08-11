76e89bb68c877d58fc365de4a8ef8fd2146d17a1
LeeSimonH <simonlee1125@gmail.com>
2025-08-11 03:54:08 -0400

    refactor(app): update layout, header/footer, and global styles

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,15 +1,32 @@
 'use client'
-
+import { MapPinIcon, CopyrightIcon } from 'lucide-react'
 import ThemeSwitch from '@/components/theme-switch'
 
 export function Footer() {
   return (
-    <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
-      <div className="flex items-center justify-between">
-        <a href="https://github.com/leesimonh" target="_blank">
-          <span className="text-xs text-zinc-500">© 2025 Simon H Lee</span>
+    <footer className="text-secondary border-muted border-t px-0 py-8 dark:border-zinc-800">
+      <div className="relative flex min-w-full items-center justify-end gap-6 text-xs tracking-tight">
+        <div className="absolute left-0 text-xs">
+          <ThemeSwitch />
+        </div>
+
+        <div className="inline-flex content-center items-center gap-1">
+          <MapPinIcon size={12} />
+          <span>Brooklyn, NY</span>
+        </div>
+
+        <a
+          href="https://github.com/leesimonh"
+          target="_blank"
+          className="inline-flex items-center justify-end gap-0.5"
+        >
+          <CopyrightIcon size={12} />
+          2025 Simon H Lee
         </a>
-        <div className="text-xs text-zinc-400">
+      </div>
+
+      <div className="hidden min-w-full grid-cols-12 place-items-end gap-8 text-xs tracking-tight">
+        <div className="col-span-2 text-xs">
           <ThemeSwitch />
         </div>
       </div>
