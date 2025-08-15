f168d35dbda0c0779087e50949e60413cc954e3f
LeeSimonH <simonlee1125@gmail.com>
2025-08-14 23:12:56 -0400

    style(theme): refresh theme and UI components styles; add Tailwind v4 globals and utilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 'use client'
 import { AnimatedBackground } from '@/components/ui/animated-background'
 import { Magnetic } from '@/components/ui/magnetic'
-// import {
-//   MorphingDialog,
-//   MorphingDialogClose,
-//   MorphingDialogContainer,
-//   MorphingDialogContent,
-//   MorphingDialogTrigger,
-// } from '@/components/ui/morphing-dialog'
 import ExperiencesTimeline from '@/components/vertical-timeline'
 import { motion } from 'motion/react'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-// import { InstagramEmbed } from 'react-social-media-embed'
 import { BLOG_POSTS as FALLBACK_BLOG_POSTS, SOCIAL_LINKS, WORK_EXPERIENCE } from './data'
-// import { ContactFormInline } from '@/components/contact-form-inline'
+import { ContactFormInline } from '@/components/contact-form-inline'
 
 const VARIANTS_CONTAINER = {
   hidden: { opacity: 0 },
@@ -110,16 +102,6 @@ export default function Personal() {
       uid: string
     }[]
   )
-  // const [isModalOpen, setIsModalOpen] = useStatecbooleane(false)
-
-  // function onContactFormClose() {
-  //   console.log('Modal closed')
-  //   setIsModalOpen(false)
-  // }
-
-  // function onContactFormSave(data: any) {
-  //   console.log('Contact saved:', data)
-  // }
 
   useEffect(() => {
     fetch('/api/blogposts')
@@ -180,73 +162,7 @@ export default function Personal() {
             </span>
           </p>
         </div>
-
         {/* MARK: Social Chips/Hovers/Cards */}
-        {/* <div className="text-secondary flex flex-wrap gap-4 font-mono text-sm">
-          <div className="rounded-lg bg-zinc-50/40 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-900/40 dark:ring-zinc-800/50">
-            <SocialMediaVideo
-              url={
-                'https://www.instagram.com/reel/DG9IfwwSSVz/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
-              }
-            >
-              <div className="px-3 py-2">
-                <div>climb</div>
-                <Link
-                  href="https://www.instagram.com/crimpwimp/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  @crimpwimp
-                </Link>
-              </div>
-            </SocialMediaVideo>
-          </div>
-
-          <div className="rounded-lg bg-zinc-50/40 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-900/40 dark:ring-zinc-800/50">
-            <SocialMediaVideo
-              url={
-                'https://www.instagram.com/p/CoJZDoEuiPs/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
-              }
-            >
-              <div className="px-3 py-2">
-                <div>draw</div>
-                <Link
-                  href="https://www.instagram.com/nomis_heel/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  @nomis_heel
-                </Link>
-              </div>
-            </SocialMediaVideo>
-          </div>
-
-          <div className="rounded-lg bg-zinc-50/40 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-900/40 dark:ring-zinc-800/50">
-            <div className="px-3 py-2">
-              <div>snap photos</div>
-              <Link
-                href="https://vsco.co/simonhl/gallery"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                vsco/simonhl
-              </Link>
-            </div>
-          </div>
-
-          <div className="rounded-lg bg-zinc-50/40 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-900/40 dark:ring-zinc-800/50">
-            <div className="px-3 py-2">
-              <div>read</div>
-              <Link
-                href="https://www.goodreads.com/user/show/50267865-simon-lee"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                goodreads
-              </Link>
-            </div>
-          </div>
-        </div> */}
       </motion.section>
 
       {/* MARK: Work Experience */}
@@ -265,7 +181,6 @@ export default function Personal() {
           <AnimatedBackground
             enableHover
             className="h-full w-full rounded-lg"
-            // className="h-full w-full rounded-lg bg-zinc-200/40 dark:bg-zinc-900/80"
             transition={{
               type: 'spring',
               bounce: 0,
@@ -290,23 +205,6 @@ export default function Personal() {
                   </p>
                 </div>
               </Link>
-
-              // Previous card
-              // <Link
-              //   key={post.uid}
-              //   className="group -mx-3 rounded-xl px-3 py-3"
-              //   href={post.link}
-              //   data-id={post.uid}
-              // >
-              //   <div className="group flex flex-col space-y-1">
-              //     <h4 className="group-hover:text-primary-hover text-primary font-medium dark:text-zinc-100">
-              //       {post.title}
-              //     </h4>
-              //     <p className="text-secondary group-hover:text-secondary text-sm font-normal group-hover:font-normal">
-              //       {post.description}
-              //     </p>
-              //   </div>
-              // </Link>
             ))}
           </AnimatedBackground>
         </div>
@@ -320,10 +218,6 @@ export default function Personal() {
       >
         <div className="flex flex-col gap-y-4">
           <h2 className="font-heading text-lg">Connect</h2>
-          {/* Github, LinkedIn, Instagram links/pills */}
-          <h3 className="font-heading sr-only hidden" aria-hidden="true" aria-label="socials">
-            Socials
-          </h3>
 
           <div className="contact-content">
             <div className="contact-info flex flex-col gap-4">
@@ -333,34 +227,13 @@ export default function Personal() {
                   Get in touch!
                 </p>
               </div>
-
-              {/* Link-styled Button to open Contact Form */}
-              {/* <button
-                type="button"
-                className="group btn-link relative inline-flex max-w-fit items-center gap-1 font-mono"
-                onClick={() => setIsModalOpen(true)}
-              >
-                Contact me
-                <svg
-                  width="15"
-                  height="15"
-                  viewBox="0 0 15 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-3 w-3"
-                >
-                  <path
-                    d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
-                    fill="currentColor"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="absolute bottom-0.5 left-0 block h-0.5 w-full max-w-0 bg-[currentColor] transition-all duration-200 group-hover:max-w-full"></span>
-              </button> */}
             </div>
           </div>
 
+          {/* Github, LinkedIn, Instagram links/pills */}
+          <h3 className="font-heading sr-only hidden" aria-hidden="true" aria-label="socials">
+            Socials
+          </h3>
           <div className="flex flex-wrap gap-1">
             {SOCIAL_LINKS.map((link) => (
               <MagneticSocialLink key={link.label} link={link.link}>
@@ -370,24 +243,7 @@ export default function Personal() {
           </div>
         </div>
 
-        {/* <ContactFormInline /> */}
-
-        {/* <SimpleContactModal
-          isOpen={isModalOpen}
-          onClose={() => onContactFormClose()}
-          onSave={(data: any) => onContactFormSave(data)}
-          initialData={{
-            firstName: '',
-            lastName: '',
-            phoneNumber: '',
-            streetAddr: '',
-            city: '',
-            region: '',
-            country: Country.US_CA,
-            zipCode: '',
-            notes: '',
-          }}
-        /> */}
+        <ContactFormInline />
       </motion.section>
     </motion.main>
   )
--- components/vertical-timeline.tsx
+++ components/vertical-timeline.tsx
@@ -13,60 +13,62 @@ import { AsteriskIcon } from 'lucide-react'
 
 export default function ExperiencesTimeline(props: { experiences: WorkExperience[] }) {
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full max-w-[48ch]">
       <Timeline defaultValue={props.experiences.length}>
         {props.experiences.map((item, idx) => (
           <TimelineItem
             key={item.id}
             step={idx + 1}
-            className="text-body-primary group-data-[orientation=vertical]/timeline:ms-10"
+            className="text-primary group-data-[orientation=vertical]/timeline:ms-10"
           >
             <TimelineHeader>
               <TimelineSeparator className="border border-zinc-300 group-data-[orientation=vertical]/timeline:-left-8 group-data-[orientation=vertical]/timeline:h-[calc(100%-2rem)] group-data-[orientation=vertical]/timeline:translate-y-8 dark:border-zinc-600" />
 
-              <TimelineTitle className="font-sans text-base">
-                <div>
+              <TimelineTitle>
+                <div className="flex w-full items-baseline justify-between">
                   {/* MARK: Job Title */}
-                  <h4 className="text-lg tracking-tight">{item.title}</h4>
+                  <span className="text-base font-medium">{item.title}</span>
 
-                  {/* MARK: Company Name */}
-                  <a
-                    href={item.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="group text-body-secondary relative inline-block transition-colors duration-200 hover:bg-transparent"
-                  >
-                    <span className="group relative inline-flex shrink-1 items-center gap-0.5">
-                      {item.company}{' '}
-                      <svg
-                        width="15"
-                        height="15"
-                        viewBox="0 0 15 15"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="inline h-3 w-3"
-                      >
-                        <path
-                          d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
-                          fill="currentColor"
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                      <span className="dark:bg-accent absolute bottom-0.5 left-0 block h-0.5 w-full max-w-0 bg-[currentColor] transition-all duration-200 group-hover:max-w-full"></span>
-                    </span>
-                  </a>
+                  {/* MARK: Job -> Dates */}
+                  <TimelineDate>
+                    <div
+                      className="flex text-sm font-normal text-zinc-500"
+                      style={{ wordSpacing: '-0.02em' }}
+                    >
+                      {item.start} - {item.end}
+                    </div>
+                  </TimelineDate>
                 </div>
+
+                {/* MARK: Company Name */}
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="group relative inline-block hover:bg-transparent"
+                >
+                  <span className="group text-secondary/90 hover:text-secondary-hover/90 relative inline-flex shrink-1 items-center gap-0.5 text-base leading-snug font-normal transition-colors duration-300 ease-in-out">
+                    {item.company}{' '}
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <span className="bg-accent-hover absolute bottom-0.5 left-0 block h-px w-full max-w-0 transition-all duration-300 group-hover:max-w-full"></span>
+                  </span>
+                </a>
               </TimelineTitle>
 
-              {/* MARK: Job -> Dates */}
-              <TimelineDate>
-                <div className="text-body-muted flex font-normal">
-                  {item.start} - {item.end}
-                </div>
-              </TimelineDate>
-
-              <TimelineIndicator className="group-data-completed/timeline-item:bg group-data-completed/timeline-item:text-secondary flex size-8 items-center justify-center group-data-completed/timeline-item:border-0 group-data-[orientation=vertical]/timeline:-left-8">
+              <TimelineIndicator className="flex size-8 items-center justify-center group-data-completed/timeline-item:border-0 group-data-[orientation=vertical]/timeline:-left-8">
                 <AsteriskIcon className="group-not-data-completed/timeline-item:hidden" size={20} />
               </TimelineIndicator>
             </TimelineHeader>
@@ -79,7 +81,7 @@ export default function ExperiencesTimeline(props: { experiences: WorkExperience
                     {item.highlights.map((highlight, idx) => (
                       <li
                         key={`${item.id}-highlight-${idx}`}
-                        className="mb-4 font-mono text-sm leading-normal text-pretty"
+                        className="mb-4 font-sans text-sm leading-normal text-pretty"
                       >
                         {highlight}
                       </li>
